a3218723345d515d95d14f23b37bb2cab1ecd621
Temiloluwa <akintadetemi19@gmail.com>
2025-04-02 15:12:36 +0100

    added swiping effect project

--- mini-projects/github-api/index.js
+++
@@ -1,7 +0,0 @@
-async function getRepoData(name) {
-    fetch(`https://api.github.com/repos/Temi-Tade/${name}/languages`)
-    .then((res) => res.json())
-    .then((data) => console.log(Object.keys(data)))
-}
-
-getRepoData("beautynes")
